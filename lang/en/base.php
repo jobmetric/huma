746a78b05dc86fc2e7a33acae657007e94ab9f9a
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-16 21:03:41 +0330

    added language key

---
+++ lang/en/base.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Huma Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Huma for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'description' => 'A user-friendly HRM platform designed to empower human resources, streamline employee management, and foster a positive workplace culture.',
+
+    'sections' => [
+        'attendance' => [
+            'name' => 'Attendance',
+            'title' => 'Attendance Management',
+        ],
+        'salary' => [
+            'name' => 'Salary',
+            'title' => 'Salary Management',
+        ],
+        'feature' => [
+            'name' => 'Features',
+            'title' => 'Features Management',
+        ],
+        'employee' => [
+            'name' => 'Employees',
+            'title' => 'Employees Management',
+        ],
+        'system' => [
+            'name' => 'System',
+            'title' => 'System Management',
+        ],
+        'report' => [
+            'name' => 'Reports',
+            'title' => 'Management Reports',
+        ],
+    ],
+
+];
