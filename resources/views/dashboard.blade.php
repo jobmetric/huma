eba4f29567391b6415a3c706f5e89c81575860fd
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-23 15:02:10 +0330

    changed tile with tile statistics in panelio component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,115 +3,55 @@
 @section('body')
     <div class="row gy-5 g-xl-10">
         <div class="col-sm-6 col-xl-2 mb-xl-10">
-            <div class="card h-lg-100">
-                <div class="card-body d-flex justify-content-between align-items-start flex-column">
-                    <div class="m-0">
-                        <i class="ki-duotone ki-compass fs-2hx text-gray-600">
-                            <span class="path1"></span>
-                            <span class="path2"></span>
-                        </i>
-                    </div>
-                    <div class="d-flex flex-column my-2">
-                        <span class="fw-semibold fs-3x text-gray-800 lh-3 ls-n2">61</span>
-                        <div class="m-0">
-                            <span class="fw-semibold fs-6 text-gray-400">{{ trans('huma::base.dashboard.tiles.employees') }}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <x-tile-statistics name="{{ trans('huma::base.dashboard.tiles.employees') }}" number="61">
+                <i class="ki-duotone ki-compass fs-2hx text-gray-600">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                </i>
+            </x-tile-statistics>
         </div>
         <div class="col-sm-6 col-xl-2 mb-xl-10">
-            <div class="card h-lg-100">
-                <div class="card-body d-flex justify-content-between align-items-start flex-column">
-                    <div class="m-0">
-                        <i class="ki-duotone ki-chart-simple fs-2hx text-gray-600">
-                            <span class="path1"></span>
-                            <span class="path2"></span>
-                            <span class="path3"></span>
-                            <span class="path4"></span>
-                        </i>
-                    </div>
-                    <div class="d-flex flex-column my-2">
-                        <span class="fw-semibold fs-3x text-gray-800 lh-3 ls-n2">5</span>
-                        <div class="m-0">
-                            <span class="fw-semibold fs-6 text-gray-400">{{ trans('huma::base.dashboard.tiles.departments') }}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <x-tile-statistics name="{{ trans('huma::base.dashboard.tiles.departments') }}" number="5">
+                <i class="ki-duotone ki-chart-simple fs-2hx text-gray-600">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                    <span class="path3"></span>
+                    <span class="path4"></span>
+                </i>
+            </x-tile-statistics>
         </div>
         <div class="col-sm-6 col-xl-2 mb-xl-10">
-            <div class="card h-lg-100">
-                <div class="card-body d-flex justify-content-between align-items-start flex-column">
-                    <div class="m-0">
-                        <i class="ki-duotone ki-abstract-39 fs-2hx text-gray-600">
-                            <span class="path1"></span>
-                            <span class="path2"></span>
-                        </i>
-                    </div>
-                    <div class="d-flex flex-column my-2">
-                        <span class="fw-semibold fs-3x text-gray-800 lh-3 ls-n2">50</span>
-                        <div class="m-0">
-                            <span class="fw-semibold fs-6 text-gray-400">{{ trans('huma::base.dashboard.tiles.today_attendees') }}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <x-tile-statistics name="{{ trans('huma::base.dashboard.tiles.today_attendees') }}" number="50">
+                <i class="ki-duotone ki-abstract-39 fs-2hx text-gray-600">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                </i>
+            </x-tile-statistics>
         </div>
         <div class="col-sm-6 col-xl-2 mb-xl-10">
-            <div class="card h-lg-100">
-                <div class="card-body d-flex justify-content-between align-items-start flex-column">
-                    <div class="m-0">
-                        <i class="ki-duotone ki-map fs-2hx text-gray-600">
-                            <span class="path1"></span>
-                            <span class="path2"></span>
-                            <span class="path3"></span>
-                        </i>
-                    </div>
-                    <div class="d-flex flex-column my-2">
-                        <span class="fw-semibold fs-3x text-gray-800 lh-3 ls-n2">2</span>
-                        <div class="m-0">
-                            <span class="fw-semibold fs-6 text-gray-400">{{ trans('huma::base.dashboard.tiles.absent_today') }}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <x-tile-statistics name="{{ trans('huma::base.dashboard.tiles.absent_today') }}" number="2">
+                <i class="ki-duotone ki-map fs-2hx text-gray-600">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                    <span class="path3"></span>
+                </i>
+            </x-tile-statistics>
         </div>
         <div class="col-sm-6 col-xl-2 mb-5 mb-xl-10">
-            <div class="card h-lg-100">
-                <div class="card-body d-flex justify-content-between align-items-start flex-column">
-                    <div class="m-0">
-                        <i class="ki-duotone ki-abstract-35 fs-2hx text-gray-600">
-                            <span class="path1"></span>
-                            <span class="path2"></span>
-                        </i>
-                    </div>
-                    <div class="d-flex flex-column my-2">
-                        <span class="fw-semibold fs-3x text-gray-800 lh-3 ls-n2">9</span>
-                        <div class="m-0">
-                            <span class="fw-semibold fs-6 text-gray-400">{{ trans('huma::base.dashboard.tiles.person_on_leave') }}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <x-tile-statistics name="{{ trans('huma::base.dashboard.tiles.person_on_leave') }}" number="9">
+                <i class="ki-duotone ki-abstract-35 fs-2hx text-gray-600">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                </i>
+            </x-tile-statistics>
         </div>
         <div class="col-sm-6 col-xl-2 mb-5 mb-xl-10">
-            <div class="card h-lg-100">
-                <div class="card-body d-flex justify-content-between align-items-start flex-column">
-                    <div class="m-0">
-                        <i class="ki-duotone ki-abstract-26 fs-2hx text-gray-600">
-                            <span class="path1"></span>
-                            <span class="path2"></span>
-                        </i>
-                    </div>
-                    <div class="d-flex flex-column my-2">
-                        <span class="fw-semibold fs-3x text-gray-800 lh-3 ls-n2">999M</span>
-                        <div class="m-0">
-                            <span class="fw-semibold fs-6 text-gray-400">{{ trans('huma::base.dashboard.tiles.total_salary') }}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <x-tile-statistics name="{{ trans('huma::base.dashboard.tiles.total_salary') }}" number="599999999">
+                <i class="ki-duotone ki-abstract-26 fs-2hx text-gray-600">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                </i>
+            </x-tile-statistics>
         </div>
     </div>
 @endsection
